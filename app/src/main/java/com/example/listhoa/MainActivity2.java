e43693dbe1f3f67c0858cfdc915442cd18245a8d
kietkiet2332000 <kietkiet2332000@gmail.com>
2021-11-08 20:59:19 +0700

    commit1

---
+++ app/src/main/java/com/example/listhoa/MainActivity2.java
@@ -0,0 +1,22 @@
+package com.example.listhoa;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.view.ViewCompat;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.transition.Fade;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
